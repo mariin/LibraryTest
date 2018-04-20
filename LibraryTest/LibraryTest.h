753ddabfa63a76c322374b0b3f285b72537b9e60
marie <marie@nicolaudie.com>
2018-04-20 13:45:50 +0200

    Initial commit

---
+++ LibraryTest/LibraryTest.h
@@ -0,0 +1,19 @@
+//
+//  LibraryTest.h
+//  LibraryTest
+//
+//  Created by Nicolaudie on 20/04/2018.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibraryTest.
+FOUNDATION_EXPORT double LibraryTestVersionNumber;
+
+//! Project version string for LibraryTest.
+FOUNDATION_EXPORT const unsigned char LibraryTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibraryTest/PublicHeader.h>
+
+
---
+++ LibraryTest/SuperClass.swift
@@ -0,0 +1,18 @@
+//
+//  SuperClass.swift
+//  LibraryTest
+//
+//  Created by Nicolaudie on 20/04/2018.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import Foundation
+
+class SuperClass {
+    
+    func printHelloWord () {
+        print ("Hello Word !!!")
+    }
+    
+    
+}
